400c4fc5c50601253a1c7005200dcdda20c411f6
Aditya Wadaskar <wadaskar.aditya@gmail.com>
2020-01-30 14:44:53 -0800

    Add sponsors page

--- static/img/sponsors/format_images.sh
+++ static/img/sponsors/format_images.sh
@@ -1,7 +1,6 @@
-mkdir current_sponsors_tmp
-mogrify -format png -path ./current_sponsors_tmp ./current_sponsors/*
-rm -r current_sponsors
-mv current_sponsors_tmp current_sponsors
-mogrify -resize 100x100 -background white -gravity center -extent 100x100 -path ./current_sponsors_resized ./current_sponsors/*.png
-cp -R current_sponsors/. all_sponsors/.
-cp -R current_sponsors_resized/. all_sponsors_resized/.
+mkdir sponsors_tmp
+mogrify -format png -path ./sponsors_tmp ./sponsors_original/*
+rm -r sponsors_original
+mv sponsors_tmp sponsors_original
+mogrify -resize 100x100 -background white -gravity center -extent 100x100 -path ./sponsors_small ./sponsors_original/*.png
+mogrify -resize 500x200 -background white -gravity center -extent 500x200 -path ./sponsors_large ./sponsors_original/*.png
